1ba6f90d22fbbee0120ef5e64e7d9264bbf7f660
Mish_Sanek <kid02@list.ru>
2020-01-27 12:17:28 +0500

    6. Личный проект: больше деталей (часть 1)

---
+++ js/main.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var picturesWrapper = document.querySelector('.pictures');
+var commentFragment = document.createDocumentFragment();
+
+var dataArray = [];
+var commentsArray = [];
+var namesList = ['Стояна', 'Лежана', 'Вынька', 'Бздашек', 'Мыкола', 'Маразмус', 'Гиви', 'Вага', 'Вацлав'];
+var messagesList = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+function getRandomInRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+for (var i = 0; i <= 24; i++) {
+  dataArray.push({
+    url: 'photos/' + (i + 1) + '.jpg',
+    description: '',
+    likes: getRandomInRange(15, 200)
+  });
+}
+
+for (var i = 0; i <= 24; i++) {
+  commentsArray.push({
+    avatar: 'img/avatar-' + getRandomInRange(1, 6) + '.svg',
+    message: messagesList[getRandomInRange(0, messagesList.length)],
+    name: namesList[getRandomInRange(0, namesList.length)],
+  });
+}
+
+for (var i = 0; i <= 24; i++) {
+  var commentElement = document.createElement('a');
+  commentElement.classList.add('picture');
+  var commentAvatar = document.createElement('img');
+  commentAvatar.setAttribute('src', dataArray[i].url);
+  commentAvatar.setAttribute('class', 'picture__img');
+  var commentInfo = document.createElement('p');
+  commentInfo.classList.add('picture__info');
+  var commentsLikes = document.createElement('span');
+  commentsLikes.classList.add('picture__likes');
+  commentsLikes.textContent = dataArray[i].likes;
+  var commentsCount = document.createElement('span');
+  commentsCount.classList.add('picture__comments');
+  commentsCount.textContent = getRandomInRange(0, messagesList.length);
+
+  commentElement.appendChild(commentAvatar);
+  commentElement.appendChild(commentInfo);
+  commentInfo.appendChild(commentsLikes);
+  commentInfo.appendChild(commentsCount);
+  commentFragment.appendChild(commentElement);
+}
+
+picturesWrapper.appendChild(commentFragment);
